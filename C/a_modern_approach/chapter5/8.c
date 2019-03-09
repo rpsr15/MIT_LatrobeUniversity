d43ead444b1b07c2e60bd8f03865d60f4daea3b0
Ravi <ravipratapsinghrathore15@gmail.com>
2019-03-09 19:56:10 +1100

    prob

---
+++ C/a_modern_approach/chapter5/8.c
@@ -0,0 +1,13 @@
+//fine closest departure time
+
+#include <stdio.h>
+int main(void)
+{
+    int hour,minute;
+    printf("Enter a 24-hour time:\n");
+    //read 13:15
+    scanf("%d:%d", &hour, &minute);
+    
+
+    return 0;
+}
